115c8caf706d40e671feef38344b0ebebcd02c51
佟瑶瑶 <ty18342913016@163.com>
2020-01-10 15:55:50 +0800

    初始化

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="innerBox">
+             {{startData}}
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            startData:'hello world'
+        }
+    }
+}
+</script>
+<style lang="less">
+    .innerBox {
+        color: red;
+    }
+</style>
